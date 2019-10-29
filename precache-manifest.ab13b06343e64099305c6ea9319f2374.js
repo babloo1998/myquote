2c225897ef418836eb668c5cab3b4b4a44327044
Babloo Mishra <babloo.mishra@tothenew.com>
2019-10-29 15:35:17 +0530

    Updates

---
+++ precache-manifest.ab13b06343e64099305c6ea9319f2374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf5b66623804c6ec0fb619af24dd090",
+    "url": "/myquote/index.html"
+  },
+  {
+    "revision": "da60295ef93b6fc14ce9",
+    "url": "/myquote/static/css/main.871c03de.chunk.css"
+  },
+  {
+    "revision": "790cde14fedefcba2bed",
+    "url": "/myquote/static/js/2.c67c5bc0.chunk.js"
+  },
+  {
+    "revision": "da60295ef93b6fc14ce9",
+    "url": "/myquote/static/js/main.6551d36f.chunk.js"
+  },
+  {
+    "revision": "4a7e5141146df8ec30a1",
+    "url": "/myquote/static/js/runtime-main.06d861aa.js"
+  }
+]);
